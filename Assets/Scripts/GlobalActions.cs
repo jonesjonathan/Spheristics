587ea3d1350546089a64f32b737926d079e4bf40
jonesjonathan <jonesjon@oregonstate.edu>
2018-03-13 09:41:29 -0700

    Added timer and created additional elements
    Timer added to UI and some additional objects created. Easier level
    creation.

--- Assets/Scripts/GlobalActions.cs
+++ Assets/Scripts/GlobalActions.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GlobalActions : MonoBehaviour {
 
     public int currentScene;
     public int nextScene;
+    public Text timerText;
+    private float timer = 0;
     private string[] scenes = { "devBox", "Level2" };
 	// Update is called once per frame
 	void Update () {
+        //Timer
+        timer += Time.deltaTime;
+        timerText.text = timer.ToString("0.00") + "s";
+
 		if(Input.GetKeyDown("r"))
         {
             SceneManager.LoadScene(scenes[currentScene]);
